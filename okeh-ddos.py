9265ad7014678f38d7ff54958a8683e8d544647f
Okehuy <141473753+Okehuy@users.noreply.github.com>
2023-08-08 17:31:56 +0700

    Update okeh-ddos.py

--- okeh-ddos.py
+++ okeh-ddos.py
@@ -23,7 +23,7 @@ print("           0ppppppp0      0  0      0000000      0     0        ")
 print("THIS TOOLS DONT ATTACK GOV")
 
  ip = raw_input("IP Target : ")
- port = input("Port   : ")
+ port = input("Port       : ")
  
  os.system("clear")
  while True:
